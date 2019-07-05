d62c0eff8d877d51643c03dfb8ae021fe853497b
sevenquarkoniums <sevenquarkoniums@gmail.com>
2019-07-04 18:21:45 -0700

    working version

--- miniDota.py
+++ miniDota.py
@@ -23,18 +23,21 @@ Transition = namedtuple('Transition', ('state', 'action', 'nextstate', 'reward')
 def main():
     m = miniDota(saveModel=1, loadModel=1)
     m.train()
-#    m.test()
+#    m.test(randomness=0.1)
+    m.behavior()
     del m
 
 class miniDota:
     def __init__(self, saveModel, loadModel):
-        self.episodeNum = 3000
-        self.trainPerEpisode = 10
-        self.batchSize = 256
+        self.episodeNum = 600
+        self.trainPerEpisode = 8
+        self.batchSize = 32
         self.learningRate = 0.001
         self.gamma = 0.999
-        self.memorySize = 10000
-        self.thresStart, self.thresEnd, self.thresDecay = 0.3, 0, 1000
+        self.memorySize = 100000
+        self.thresStart, self.thresEnd, self.thresDecay = 0.2, 0.05, 200
+        self.showPeriod = 10
+        self.targetUpdatePeriod = 4
         
         self.saveModel = saveModel
         self.loadModel = loadModel
@@ -49,16 +52,18 @@ class miniDota:
         self.targetNet.eval()
         if self.loadModel:
             self.net.load_state_dict(torch.load('miniDota.pt'))
-        self.optimizer = optim.SGD(self.net.parameters(), lr=self.learningRate, momentum=0.9, weight_decay=1e-5)
+#        self.optimizer = optim.SGD(self.net.parameters(), lr=self.learningRate, momentum=0.9, weight_decay=1e-5)
+#        self.optimizer = optim.RMSprop(self.net.parameters(), lr=self.learningRate, alpha=0.95, eps=1e-8, weight_decay=1e-5, momentum=0.95, centered=False)
+        self.optimizer = optim.Adam(self.net.parameters(), lr=self.learningRate, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5, amsgrad=False)
         self.memory = replayMemory(self.memorySize)
         start = time.time()
         losses = []
         lastTen = []
         for episode in range(self.episodeNum):
             self.actionThres = self.thresEnd + (self.thresStart - self.thresEnd) * math.exp(-episode/self.thresDecay)
-            if episode % 10 == 0:
-                result, record = self.playGame(recording=1)
-                self.draw(record)
+            if episode % self.showPeriod == 0:
+                result, record, baseX, baseY = self.playGame(recording=1)
+                self.draw(record, baseX, baseY)
                 print(result)
             else:
                 self.playGame(recording=0)
@@ -73,7 +78,8 @@ class miniDota:
                 lastTen = []
             if self.saveModel and (episode+1) % 100 == 0:
                 torch.save(self.net.state_dict(), 'miniDota.pt')
-            self.targetNet.load_state_dict(self.net.state_dict())
+            if (episode+1) % self.targetUpdatePeriod == 0:
+                self.targetNet.load_state_dict(self.net.state_dict())
         end = time.time()
         print('Time: %.f s' % (end-start))
         
@@ -107,7 +113,7 @@ class miniDota:
                 self.memory.push(state, action.unsqueeze(1), nextstate, reward)
         if recording:
             record = torch.cat(record)
-        return result, record
+        return result, record, game.baseX, game.baseY
 
     def optimize(self, iterate=1):
         for i in range(iterate):
@@ -122,7 +128,8 @@ class miniDota:
             values = self.net(stateBatch).gather(1, actionBatch)
             nextstateValues = self.targetNet(nextstateBatch).max(1)[0]
             expectedValues = (nextstateValues * self.gamma) + rewardBatch
-            loss = F.smooth_l1_loss(values, expectedValues.unsqueeze(1))
+#            loss = F.smooth_l1_loss(values, expectedValues.unsqueeze(1))
+            loss = nn.MSELoss(size_average=None, reduce=None, reduction='mean')(values, expectedValues.unsqueeze(1))
             self.optimizer.zero_grad()
             loss.backward()
             for param in self.net.parameters():
@@ -130,24 +137,24 @@ class miniDota:
             self.optimizer.step()
         return loss
 
-    def test(self):
+    def test(self, randomness=0.05):
         self.net = RLNet().to(DEVICE)
         self.net.load_state_dict(torch.load('miniDota.pt'))
-        self.actionThres = 0.05
-        result, record = self.playGame(recording=1)
-        self.draw(record)
+        self.actionThres = randomness
+        result, record, baseX, baseY = self.playGame(recording=1)
+        self.draw(record, baseX, baseY)
         print(result)
         
-    def draw(self, record):
+    def draw(self, record, baseX, baseY):
         if os.name == 'nt':
-            fig, ax = plt.subplots(figsize=(7,6))
+            fig, ax = plt.subplots(figsize=(6,5))
             x, y, attack = record[:, 0].tolist(), record[:, 1].tolist(), record[:, 3].tolist()
             ax.plot(x, y, '-')
             ax.set_xlabel('X')
             ax.set_ylabel('Y')
             ax.set_xlim(-1, 51)
             ax.set_ylim(-1, 51)
-            ax.plot(30, 40, '*r', markersize=10)
+            ax.plot(baseX, baseY, '*r', markersize=10)
             attX, attY = [], []
             for idx, a in enumerate(attack):
                 if a == 1:
@@ -155,12 +162,46 @@ class miniDota:
                     attY.append(y[idx])
             ax.plot(attX, attY, 'x', color='chocolate')
             plt.show()
+            
+    def behavior(self):
+        self.net = RLNet().to(DEVICE)
+        self.net.load_state_dict(torch.load('miniDota.pt'))
+        
+        for enemyBaseHealth in [100, 50, 0]:
+            allinput = []
+            for posX in range(51):
+                for posY in range(51):
+                    allinput.append([posX, posY, enemyBaseHealth])
+            with torch.no_grad():
+                values = self.net.forward(torch.tensor(allinput, device=DEVICE, dtype=torch.float))
+            actions = torch.argmax(values, dim=1).tolist()
+            fig, ax = plt.subplots(figsize=(12,12))
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_xlim(-1, 51)
+            ax.set_ylim(-1, 51)
+            ax.plot(30, 30, '*r', markersize=10)
+            for idx, act in enumerate(actions):
+                halfsize, headW, headL = 0.3, 0.3, 0.2
+                x, y = idx // 51, idx % 51
+                if act % 5 == 1:
+                    ax.arrow(x+halfsize, y, -2*halfsize, 0, head_width=headW, head_length=headL, fc='g', ec='g')
+                elif act % 5 == 2:
+                    ax.arrow(x-halfsize, y, 2*halfsize, 0, head_width=headW, head_length=headL, fc='r', ec='r')
+                elif act % 5 == 3:
+                    ax.arrow(x, y+halfsize, 0, -2*halfsize, head_width=headW, head_length=headL, fc='c', ec='c')
+                elif act % 5 == 4:
+                    ax.arrow(x, y-halfsize, 0, 2*halfsize, head_width=headW, head_length=headL, fc='b', ec='b')
+            plt.title('EnemyBaseHealth = %d' % enemyBaseHealth)
+            plt.tight_layout()
+            plt.savefig('MoveAtHealth%d.png' % enemyBaseHealth)
+            plt.close()
 
 class gameEngine:
     def __init__(self):
         self.xlimit, self.ylimit = 50, 50
-        self.baseX, self.baseY = 30, 40
-        self.posX, self.posY = 0, 0
+        self.baseX, self.baseY = 30, 30
+        self.posX, self.posY = 0, 0#random.randint(0, 50), random.randint(0, 50)
         self.timestamp = 0
         self.maxTime = 1000
         self.enemyBaseHealth = 100
@@ -184,13 +225,13 @@ class gameEngine:
         hitpoint = 0
         consumption = 0
         if action % 2 == 1:# attack.
-            if prevDist < 5:
+            if prevDist < 10:
                 hitpoint = 1
                 self.enemyBaseHealth -= hitpoint
                 if self.enemyBaseHealth == 0:
-                    return torch.tensor([300], device=DEVICE, dtype=torch.float), 'win'
+                    return torch.tensor([100], device=DEVICE, dtype=torch.float), 'win'# not consistent.
             else:
-                consumption += 0.5
+                consumption += 1
         self.timestamp += 1
         if self.timestamp == self.maxTime:
             return torch.tensor([0], device=DEVICE, dtype=torch.float), 'draw'
@@ -200,9 +241,9 @@ class gameEngine:
 #            self.shortestDist = newDist
 #        else:
 #            distReward = 0
-        distReward = prevDist - newDist
+        distReward = -newDist/50
         attackReward = hitpoint
-        reward = 1 * distReward + 1 * attackReward - 1 * consumption
+        reward = 0.1 * distReward + 1 * attackReward - 0 * consumption
         return torch.tensor([reward], device=DEVICE, dtype=torch.float), 'on'
 
     def __dist__(self, x1, y1, x2, y2):
@@ -217,8 +258,10 @@ class RLNet(nn.Module):
         self.sm = nn.Softmax(1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        y = torch.empty(x.size(), device=DEVICE)
+        y[:, :2] = x[:, :2] / 25 - 1
+        y[:, 2] = x[:, 2] / 50 - 1
+        x = F.relu(self.fc1(y))
         x = F.relu(self.fc2(x))
         x = self.sm(self.fc3(x))
         return x
