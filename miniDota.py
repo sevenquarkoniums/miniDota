134e4c7f510ce5e3de7ce01ca5816c5f05bf80e7
sevenquarkoniums <sevenquarkoniums@gmail.com>
2019-07-04 23:06:20 -0700

    ppo not completed

--- miniDota.py
+++ miniDota.py
@@ -5,23 +5,29 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.distributions import Categorical
 import time
 import math
 import random
 from collections import namedtuple
 import matplotlib.pyplot as plt
+import numpy as np
 from sys import exit
 import os
-if os.name == 'nt':
-    import pygame
-#random.seed(0)
-#torch.manual_seed(0)
+#if os.name == 'nt':
+#    import pygame
+random.seed(0)
+torch.manual_seed(0)
 
+method = 'dqn' # dqn, ppo.
 DEVICE = torch.device('cuda')
-Transition = namedtuple('Transition', ('state', 'action', 'nextstate', 'reward'))
+if method == 'dqn':
+    Transition = namedtuple('Transition', ('state', 'action', 'nextstate', 'reward'))
+elif method == 'ppo':
+    Transition = namedtuple('Transition', ('state', 'action', 'advantage', 'value', 'net_log_pi'))
 
 def main():
-    m = miniDota(saveModel=1, loadModel=1)
+    m = miniDota(saveModel=0, loadModel=0)
     m.train()
 #    m.test(randomness=0.1)
     m.behavior()
@@ -30,12 +36,12 @@ def main():
 class miniDota:
     def __init__(self, saveModel, loadModel):
         self.episodeNum = 600
-        self.trainPerEpisode = 8
+        self.trainPerEpisode = 1
         self.batchSize = 32
-        self.learningRate = 0.001
+        self.learningRate = 0.01
         self.gamma = 0.999
-        self.memorySize = 100000
-        self.thresStart, self.thresEnd, self.thresDecay = 0.2, 0.05, 200
+        self.memorySize = 100000 #may clear memory for ppo.
+        self.thresStart, self.thresEnd, self.thresDecay = 1, 0.05, 200
         self.showPeriod = 10
         self.targetUpdatePeriod = 4
         
@@ -46,12 +52,16 @@ class miniDota:
         torch.cuda.empty_cache()
 
     def train(self):
-        self.net = RLNet().to(DEVICE)
-        self.targetNet = RLNet().to(DEVICE)
-        self.targetNet.load_state_dict(self.net.state_dict())
-        self.targetNet.eval()
-        if self.loadModel:
-            self.net.load_state_dict(torch.load('miniDota.pt'))
+        if method == 'dqn':
+            self.net = RLNet().to(DEVICE)
+            self.targetNet = RLNet().to(DEVICE)
+            self.targetNet.load_state_dict(self.net.state_dict())
+            self.targetNet.eval()
+            if self.loadModel:
+                self.net.load_state_dict(torch.load('miniDota.pt'))
+        elif method == 'ppo':
+            self.net = PPONet().to(DEVICE)
+        
 #        self.optimizer = optim.SGD(self.net.parameters(), lr=self.learningRate, momentum=0.9, weight_decay=1e-5)
 #        self.optimizer = optim.RMSprop(self.net.parameters(), lr=self.learningRate, alpha=0.95, eps=1e-8, weight_decay=1e-5, momentum=0.95, centered=False)
         self.optimizer = optim.Adam(self.net.parameters(), lr=self.learningRate, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5, amsgrad=False)
@@ -67,7 +77,7 @@ class miniDota:
                 print(result)
             else:
                 self.playGame(recording=0)
-            loss = self.optimize(iterate=self.trainPerEpisode)
+            loss = self.optimize(iterate=self.trainPerEpisode, clip_range=0.1)
             if loss is not None:
                 losses.append(loss)
                 lastTen.append(loss)
@@ -95,48 +105,109 @@ class miniDota:
         record = []
         while not episodeEnd:
             state = game.getState()
+            self.state = state
+            
             sample = random.random()
             if sample > self.actionThres: # use network to select move.
                 with torch.no_grad():
-                    values = self.net.forward(state)
-                action = torch.argmax(values, dim=1)
+                    if method == 'dqn':
+                        values = self.net.forward(state)
+                        action = torch.argmax(values, dim=1)
+                    elif method == 'ppo':
+                        pi, value = self.net.forward(state)
             else: # random move.
                 action = torch.randint(0, 10, (1, ), device=DEVICE)
+            
             reward, result = game.update(action.item())
-            if result in ['win', 'draw']:
-                episodeEnd = True
             nextstate = game.getState()
-            if recording:
+            
+            if recording: # not pushing to training set.
                 attack = torch.tensor([[1]], device=DEVICE, dtype=torch.float) if action.item() % 2 == 1 else torch.tensor([[0]], device=DEVICE, dtype=torch.float)
                 record.append(torch.cat([state, attack], dim=1))
             else:
-                self.memory.push(state, action.unsqueeze(1), nextstate, reward)
+                if method == 'dqn':
+                    self.memory.push(state, action.unsqueeze(1), nextstate, reward)
+                elif method == 'ppo':
+                    advantage = self._calc_advantages(result, reward, value)
+                    neg_log_pi = -pi.log_prob(pi.sample())
+                    self.memory.push(state, action, advantage, value, neg_log_pi)
+
+            if result in ['win', 'draw']:
+                episodeEnd = True
+        
         if recording:
             record = torch.cat(record)
         return result, record, game.baseX, game.baseY
 
-    def optimize(self, iterate=1):
+    def optimize(self, iterate, clip_range):
         for i in range(iterate):
             if len(self.memory) < self.batchSize:
                 return
             transitions = self.memory.sample(self.batchSize)
             batch = Transition(*zip(*transitions))
-            stateBatch = torch.cat(batch.state)
-            actionBatch = torch.cat(batch.action)
-            nextstateBatch = torch.cat(batch.nextstate)
-            rewardBatch = torch.cat(batch.reward)
-            values = self.net(stateBatch).gather(1, actionBatch)
-            nextstateValues = self.targetNet(nextstateBatch).max(1)[0]
-            expectedValues = (nextstateValues * self.gamma) + rewardBatch
-#            loss = F.smooth_l1_loss(values, expectedValues.unsqueeze(1))
-            loss = nn.MSELoss(size_average=None, reduce=None, reduction='mean')(values, expectedValues.unsqueeze(1))
+            
+            if method == 'dqn':
+                stateBatch = torch.cat(batch.state)
+                actionBatch = torch.cat(batch.action)
+                nextstateBatch = torch.cat(batch.nextstate)
+                rewardBatch = torch.cat(batch.reward)
+                values = self.net(stateBatch).gather(1, actionBatch)
+                nextstateValues = self.targetNet(nextstateBatch).max(1)[0]
+                expectedValues = (nextstateValues * self.gamma) + rewardBatch
+    #            loss = F.smooth_l1_loss(values, expectedValues.unsqueeze(1))
+                loss = nn.MSELoss(size_average=None, reduce=None, reduction='mean')(values, expectedValues.unsqueeze(1))
+            elif method == 'ppo':
+                sampled_obs = torch.cat(batch.state)
+                sampled_action = torch.cat(batch.action)
+                sampled_advantage = torch.cat(batch.advantage)
+                sampled_value = torch.cat(batch.value)
+                sampled_normalized_advantage = self._normalize(sampled_advantage)
+                sampled_neg_log_pi = torch.cat(batch.net_log_pi)
+                sampled_return = sampled_value + sampled_advantage
+                pi, value = self.net.forward(sampled_obs)
+                neg_log_pi = -pi.log_prob(sampled_action)
+                ratio = torch.exp(sampled_neg_log_pi - neg_log_pi)
+                clipped_ratio = ratio.clamp(min=1.0 - clip_range, max=1.0 + clip_range)
+                policy_reward = torch.min(ratio * sampled_normalized_advantage, clipped_ratio * sampled_normalized_advantage)
+                policy_reward = policy_reward.mean()
+                clipped_value = sampled_value + (value - sampled_value).clamp(min=-clip_range, max=clip_range)
+                vf_loss = torch.max((value - sampled_return) ** 2, (clipped_value - sampled_return) ** 2)
+                vf_loss = 0.5 * vf_loss.mean()
+                entropy_bonus = pi.entropy()
+                entropy_bonus = entropy_bonus.mean()
+                loss = - ( policy_reward - 0.5 * vf_loss + 0.01 * entropy_bonus )
+            
             self.optimizer.zero_grad()
             loss.backward()
-            for param in self.net.parameters():
-                param.grad.data.clamp_(-1, 1)
+            if method == 'dqn':
+                for param in self.net.parameters():
+                    param.grad.data.clamp_(-1, 1)
+            elif method == 'ppo':
+                nn.utils.clip_grad_norm_(self.net.parameters(), max_norm=0.5)
             self.optimizer.step()
         return loss
 
+    def _calc_advantages(self, dones, rewards, values):
+        advantage = np.zeros((self.n_workers, self.worker_steps), dtype=np.float32)
+        last_advantage = 0
+        _, last_value = self.net.forward(self.state)
+        last_value = last_value.cpu().data.numpy()
+
+        for t in reversed(range(self.worker_steps)):
+            mask = 1.0 - dones[:, t]
+            last_value = last_value * mask
+            last_advantage = last_advantage * mask
+            delta = rewards[:, t] + self.gamma * last_value - values[:, t]
+            last_advantage = delta + self.gamma * self.lamda * last_advantage
+
+            advantage[:, t] = last_advantage
+
+            last_value = values[:, t]
+        return advantage
+    
+    def _normalize(array):
+        return (array - array.mean()) / (array.std() + 1e-8)
+    
     def test(self, randomness=0.05):
         self.net = RLNet().to(DEVICE)
         self.net.load_state_dict(torch.load('miniDota.pt'))
@@ -265,6 +336,24 @@ class RLNet(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.sm(self.fc3(x))
         return x
+
+class PPONet(nn.Module):
+    def __init__(self):
+        super(PPONet, self).__init__()
+        self.fc1 = nn.Linear(3, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 10)
+        self.fc4 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        y = torch.empty(x.size(), device=DEVICE)
+        y[:, :2] = x[:, :2] / 25 - 1
+        y[:, 2] = x[:, 2] / 50 - 1
+        x = F.relu(self.fc1(y))
+        x = F.relu(self.fc2(x))
+        pi = Categorical(logits=self.fc3(x))
+        value = self.fc4(x)
+        return pi, value
 
 class replayMemory(object):
 
